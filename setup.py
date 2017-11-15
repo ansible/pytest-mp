b1fa1d5162a26d6f622918a05cfaac4f02098cd3
Ryan Fitzpatrick <rfitzpat@redhat.com>
2017-11-15 09:15:30 -0500

    update setup.py test functionality and correct info

--- setup.py
+++ setup.py
@@ -1,43 +1,48 @@
 #!/usr/bin/env python
-import os
-import codecs
+import sys
+
+from setuptools.command.test import test as TestCommand
 from setuptools import setup
 
 
 version = '0.0.1'
 
 
-def read(fname):
-    file_path = os.path.join(os.path.dirname(__file__), fname)
-    return codecs.open(file_path, encoding='utf-8').read()
+class Tox(TestCommand):
+
+    user_options = []
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = ['-v', '-r']
+
+    def run_tests(self):
+        import tox
+        errno = tox.cmdline(self.tox_args)
+        sys.exit(errno)
 
 
-setup(
-    name='pytest-mp',
-    version=version,
-    author='Ryan Fitzpatrick',
-    author_email='rmfitzpatrick@gmail.com',
-    license='MIT',
-    url='https://github.com/rmfitzpatrick/pytest-mp',
-    description='A test batcher for multiprocessed Pytest runs',
-    long_description=read('README.md'),
-    py_modules=['pytest_mp'],
-    install_requires=['pytest>=3.1.1', 'psutil'],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Framework :: Pytest',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Testing',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Operating System :: OS Independent',
-        'License :: OSI Approved :: MIT License',
-    ],
-    entry_points={
-        'pytest11': [
-            'pytest-mp = pytest_mp.plugin',
-        ],
-    },
-)
+setup(name='pytest-mp',
+      version=version,
+      author='Ryan Fitzpatrick',
+      author_email='rmfitzpatrick@gmail.com',
+      license='MIT',
+      url='https://github.com/ansible/pytest-mp',
+      description='A test batcher for multiprocessed Pytest runs',
+      long_description_markdown_filename='README.md',
+      py_modules=['pytest_mp'],
+      install_requires=['pytest', 'psutil'],
+      setup_requires=['setuptools-markdown'],
+      tests_require=['pytest', 'tox'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Framework :: Pytest',
+                   'Intended Audience :: Developers',
+                   'Topic :: Software Development :: Testing',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: Implementation :: CPython',
+                   'Operating System :: OS Independent',
+                   'License :: OSI Approved :: MIT License'],
+      entry_points={'pytest11': ['pytest-mp = pytest_mp.plugin']},
+      cmdclass={'test': Tox})
